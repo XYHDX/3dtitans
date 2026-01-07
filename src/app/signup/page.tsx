fd09d81a07062ab2aefadfab89c974e3b05abe2f
RoboTronicsEVO <your-email@example.com>
2026-01-07 15:24:54 +0300

    feat: redirect to store-dashboard after google login

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -130,7 +130,7 @@ export default function SignUpPage() {
               <span className="bg-background px-2 text-muted-foreground">Or continue with</span>
             </div>
           </div>
-          <Button variant="outline" type="button" className="w-full" onClick={() => import('next-auth/react').then(mod => mod.signIn('google'))}>
+          <Button variant="outline" type="button" className="w-full" onClick={() => import('next-auth/react').then(mod => mod.signIn('google', { callbackUrl: '/store-dashboard' }))}>
             <svg className="mr-2 h-4 w-4" viewBox="0 0 24 24">
               <path
                 d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"
